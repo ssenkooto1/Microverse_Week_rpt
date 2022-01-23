c0b96f7f6ed9ca38b4ff6ffff98bd03c97bbf499
ssenkooto1 <ssenkootos74@gmail.com>
2022-01-23 10:32:38 +0300

    Index file with a togglable menu

---
+++ scripts/navtoggles.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
+  function myclosingFunction(){
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
+  function closenav() {
+    var x = document.getElementById("menu");
+    x.style.display = "none";
+  }
