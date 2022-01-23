9c9dea3b6ecb765c5d84fb0cc943b14018a2fe0e
ssenkooto1 <ssenkootos74@gmail.com>
2022-01-23 10:59:20 +0300

    Script for togglable menu

--- scripts/navtoggles.js
+++ scripts/navtoggles.js
@@ -7,16 +7,16 @@ function myFunction() {
   }
 }
 function myclosingFunction() {
-  let x = document.getElementById('menu');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
+  let xy = document.getElementById('menu');
+  if (xy.style.display === 'block') {
+    xy.style.display = 'none';
   } else {
-    x.style.display = 'block';
+    xy.style.display = 'block';
   }
 }
 function closenav() {
-  let x = document.getElementById('menu');
-  x.style.display = 'none';
+  let xz = document.getElementById('menu');
+  xz.style.display = 'none';
 }
 myFunction();
 myclosingFunction();
